437316240740169695ca07329a51095dde307474
Gurnav224 <chaudharyg856@gmail.com>
2024-08-04 19:27:58 +0530

    fixed dotenv config

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-import express, { request, response } from "express";
+import dotenv from 'dotenv'
+import express from "express";
 import mongoose  from "mongoose"
+
+
+dotenv.config()
 
 const app = express();
 
